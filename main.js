46684ee90e85d2546c810732871e538b11a649fb
pedrounas <shockwave2k11@gmail.com>
2018-09-22 01:10:11 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
-    const connect4 = new Connect4('#connect4')
-});
+function makeBoard() {
+  row = prompt("Número de filas", 6);
+  col = prompt("Número de colunas", 7);
+  var i = 0;
+  var j = 0;
+  for (i = 0; i < parseInt(row);i++) {
+    var rows = document.createElement("div");
+    console.log(rows);
+    rows.id = "row" + i;
+    document.getElementById("board").appendChild(rows)
+    for (j = 0; j < parseInt(col);j++) {
+      var cols = document.createElement("div");
+      cols.id = "col" + i + j;
+      rows.appendChild(cols);
+
+    }
+  }
+}
